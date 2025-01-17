e98e3cefe89063cb7afe00911ceddd52eccf33d3
AstroX <dreddthedeath@gmail.com>
2025-01-18 00:37:50 +0300

    Update Hwid.lua

--- Hwid.lua
+++ Hwid.lua
@@ -1,7 +1,6 @@
 --Dont Try to Troll Or Banned
 --WebHook For your Safety 
 local bannedHWID = ""
-local bannedHWID = ""
 local function checkAndKickPlayer()
     local player = game:GetService("Players").LocalPlayer
     local playerHWID = player.UserId
@@ -15,17 +14,21 @@ local playerCount = #game:GetService("Players"):GetPlayers()
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 local ExecutorUsing = identifyexecutor()
 local HttpService = game:GetService("HttpService")
-local ConsoleJobId = 'Roblox.GameLauncher.joinGameInstance(' .. game.PlaceId .. ', "' .. game.JobId .. '")'
 local Data =
 {
     ["embeds"] = {
         {
-            ["title"] = "Check Account",
+            ["title"] = "Detect Account",
             ["url"] = "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId,
             ["description"] = "```"..game.Players.LocalPlayer.DisplayName.." ```",
             ["color"] = tonumber("0xf7c74b"),
             ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1277112182743629969/1277337544463224898/Picsart_24-08-26_01-42-34-420.png?ex=66ccccca&is=66cb7b4a&hm=80a0165401d2357f1386cfa88da35a3df2427c1186b0e8b7955272f6c5f6ead9&"},
             ["fields"] = {
+            	{
+                    ["name"] = "UserName:",
+                    ["value"] = "```"..game.Players.LocalPlayer.Name.."```",
+                    ["inline"] = true
+                },
                 {
                     ["name"] = "Execute:",
                     ["value"] = "```"..ExecutorUsing.."```",
@@ -36,15 +39,16 @@ local Data =
                     ["value"] = hwid,
                     ["inline"] = true
                 },
-                {
-                    ["name"] = "Ran:",
-                    ["value"] = "```hi```", 
-                    ["inline"] = true
-                },
+        
                 {                
                     ["name"] = "Job ID:",
-                    ["value"] = ConsoleJobId,
+                    ["value"] = " " .. jobId,
                     ["inline"] = true
+                },
+                {
+                    ["name"] = "Script Teleport:",
+                    ["value"] = "\n" .. 'game:GetService("TeleportService"):TeleportToPlaceInstance('..placeId..', "'..jobId..'", game.Players.LocalPlayer)' .. "\n", 
+                    ["inline"] = true 
                 },
                 {
                     ["name"] = "Ty For Use:",
@@ -58,5 +62,5 @@ local Data =
 local Headers = {["Content-Type"] = "application/json"}
 local Encoded = HttpService:JSONEncode(Data)
 local Request = http_request or request or HttpPost or syn.request
-local Final = {Url = "https://discord.com/api/webhooks/1329670136692478023/dZySizOGSz0NrsTiK-MKrVKsaqlm2K3cPgeJ0JXMJs7NCr8iT4UBEqK2MeuW0LldfE5L", Body = Encoded, Method = "POST", Headers = Headers}
+local Final = {Url = "https://discord.com/api/webhooks/1290720995774169171/ihPompE08lQlCEN_PwYIZW8dHkCGJYID3RbQFBIzRdXyqdahKSjGOdkFK1ymLycPm4fr", Body = Encoded, Method = "POST", Headers = Headers}
 Request(Final)
