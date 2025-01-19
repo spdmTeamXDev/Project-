54ecc36feb7f5d283313db774c04f8418408f296
spdmTeamXDev <dreddthedeath@gmail.com>
2025-01-19 04:48:53 +0300

    Update Remote.lua

--- Remote.lua
+++ Remote.lua
@@ -1,1 +1,20 @@
+local Owner = {
+5620468151,
+5593555386
+}
+
+local url = game:HttpGet("https://raw.githubusercontent.com/AstroXTeam/Project-/refs/heads/main/Rank.lua")
+local success, err = pcall(function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v.Character then
+            if table.find(Owner,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[ 😼👑 ]"..v.DisplayName
+            elseif string.find(url,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[ SpdmTeam🧏 ]"..v.DisplayName
+            elseif not v.Character.Head:FindFirstChild("OriginalSize") then
+                v.Character.Humanoid.DisplayName = "[ 🙂 ]"..v.DisplayName
+            end
+        end
+    end
+end)
 
