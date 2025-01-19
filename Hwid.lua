02142260c079ea363a45f97c7aee6440689be5fa
spdmTeamXDev <dreddthedeath@gmail.com>
2025-01-19 03:44:37 +0300

    Update Hwid.lua

--- Hwid.lua
+++ Hwid.lua
@@ -9,9 +9,8 @@ local function checkAndKickPlayer()
     end
 end    
 checkAndKickPlayer()
-local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
-local MarketplaceService = game:GetService("MarketplaceService")
+local Name = LocalPlayer.Name
 
 -- Player Info
 local LocalPlayer = Players.LocalPlayer
@@ -29,68 +28,59 @@ local ConsoleJobId = 'Roblox.GameLauncher.joinGameInstance(' .. game.PlaceId .. 
 -- Game Info
 local GAMENAME = MarketplaceService:GetProductInfo(game.PlaceId).Name
 
--- Detecting Executor
-local function detectExecutor()
-    local executor = (syn and not is_sirhurt_closure and not pebc_execute and "Synapse X")
-                    or (secure_load and "Sentinel")
-                    or (pebc_execute and "ProtoSmasher")
-                    or (KRNL_LOADED and "Krnl")
-                    or (is_sirhurt_closure and "SirHurt")
-                    or (identifyexecutor():find("ScriptWare") and "Script-Ware")
-                    or "Unsupported"
-    return executor
-end
-
--- Creating Webhook Data
-local function createWebhookData()
-    local webhookcheck = detectExecutor()
-    
-    local data = {
-        ["avatar_url"] = "https://cdn.discordapp.com/attachments/1297939101235937301/1330330257542217838/Screenshot_-_1.jpg?ex=678d9619&is=678c4499&hm=5cd2e131984ba7fad645e525ad860fabf83f0be66f4ae7ada375daf0fd66edc3&",
-        ["content"] = "",
-        ["embeds"] = {
-            {
-                ["author"] = {
-                    ["name"] = "Someone executed your script",
-                    ["url"] = "https://roblox.com",
+local playerCount = #game:GetService("Players"):GetPlayers()
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+local ExecutorUsing = identifyexecutor()
+local HttpService = game:GetService("HttpService")
+local Data =
+{
+    ["embeds"] = {
+        {
+            ["title"] = "Detect Account",
+            ["url"] = "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId,
+            ["description"] = "```"..game.Players.LocalPlayer.DisplayName.." ```",
+            ["color"] = tonumber("0xf7c74b"),
+            ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1277112182743629969/1277337544463224898/Picsart_24-08-26_01-42-34-420.png?ex=66ccccca&is=66cb7b4a&hm=80a0165401d2357f1386cfa88da35a3df2427c1186b0e8b7955272f6c5f6ead9&"},
+            ["fields"] = {
+            	{
+                    ["name"] = "username:",
+                    ["value"] = Name,
+                    ["inline"] = true
                 },
-                ["description"] = string.format(
-                    "__[Player Info](https://www.roblox.com/users/%d)__" ..
-                    " **\nDisplay Name:** %s \n**Username:** %s \n**User Id:** %d" ..
-                    "**\nTime:** %s" ..
-                    "\n\n__[Game Info](https://www.roblox.com/games/%d)__" ..
-                    "\n**Game:** %s \n**Game Id**: %d \n**Exploit:** %s" ..
-                    "\n\n**JobId:**```%s```",
-                    Userid, DName, Name, Userid,
-                    tostring(os.date("%m/%d/%Y")), tostring(os.date("%X")),
-                    game.PlaceId, GAMENAME, game.PlaceId, webhookcheck,
-                     ConsoleJobId
-                ),
-                ["type"] = "rich",
-                ["color"] = tonumber("0xFFD700"), -- Change the color if you want
-                ["thumbnail"] = {
-                    ["url"] = ""
+                {
+                    ["name"] = "Execute:",
+                    ["value"] = "```"..ExecutorUsing.."```",
+                    ["inline"] = true
                 },
-            }
+                {
+                    ["name"] = "Hwid:",
+                    ["value"] = hwid,
+                    ["inline"] = true
+                },
+                
+                {                
+                    ["name"] = "Time:",
+                    ["value"] = tostring(os.date("%X")),
+                    ["inline"] = true
+                },
+        
+                {                
+                    ["name"] = "Job ID:",
+                    ["value"] = " " .. jobId,
+                    ["inline"] = true
+                },
+                
+                {
+                    ["name"] = "Ty For Use:",
+                    ["value"] = "**__AstroXHub__**",
+                    ["inline"] = true          
+                }
+            }              
         }
     }
-    return HttpService:JSONEncode(data)
-end
-
--- Sending Webhook
-local function sendWebhook(webhookUrl, data)
-    local headers = {
-        ["content-type"] = "application/json"
-    }
-
-    local request = http_request or request or HttpPost or syn.request
-    local abcdef = {Url = webhookUrl, Body = data, Method = "POST", Headers = headers}
-    request(abcdef)
-end
-
--- Replace the webhook URL with your own URL
-local webhookUrl = "https://discord.com/api/webhooks/1329954213811191913/gKQDWRFNoJjl2wIq6Ra8VazUAIx85EnKfTEXTwHa0e4gMuou3va-9hg0J53tFP_RnkrT"
-local webhookData = createWebhookData()
-
--- Sending the webhook
-sendWebhook(webhookUrl, webhookData)
+}
+local Headers = {["Content-Type"] = "application/json"}
+local Encoded = HttpService:JSONEncode(Data)
+local Request = http_request or request or HttpPost or syn.request
+local Final = {Url = "https://discord.com/api/webhooks/1290720995774169171/ihPompE08lQlCEN_PwYIZW8dHkCGJYID3RbQFBIzRdXyqdahKSjGOdkFK1ymLycPm4fr", Body = Encoded, Method = "POST", Headers = Headers}
+Request(Final)
