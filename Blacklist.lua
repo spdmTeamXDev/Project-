6956a7352f2004390593659a78b1a8fcec3d780c
SpdmTeamX <zusdrodo@gmail.com>
2025-02-07 06:08:51 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded()
-BlackList = {7837080873, 7639808387, 5416068413, 3227272858}
+BlackList = {7837080873, 7639808387, 5416068413, 3227272858, 4474298399}
 if table.find(BlackList, game:GetService("Players").LocalPlayer.UserId) then
     game:GetService("Players").LocalPlayer:Kick("Arctic Client : Blacklisted, Think this was a mistake? Join Our Telegram Server\n لقد تم تبيندك من استرو اكس هل تضن انهو خطأ يرجه دخول سيرفير تليگرام")
   else
