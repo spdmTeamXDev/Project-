977b81da4b5b28af426d7b00d60813d41c79c73f
SpdmTeamX <zusdrodo@gmail.com>
2025-03-08 06:43:06 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded()
-BlackList = {7837080873, 7639808387, 5416068413, 3227272858, 4474298399, 7903982191, 7945902429, 3021383877}
+BlackList = {7837080873, 7639808387, 5416068413, 3227272858, 4474298399, 7903982191, 7945902429, 3021383877, 7990357915}
 if table.find(BlackList, game:GetService("Players").LocalPlayer.UserId) then
     game:Shutdown()
   else
